8917cdaaa7cad187836cad9a3485eec8a41fe87c
Vicky Thumbi <vickycrowny5@gmail.com>
2022-01-25 12:38:58 -0500

    full commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+alert("Mustang");  
+
+for ( i =1; i<=100; i++) {
+	if(i%5==0 && i%3==0){
+		console.log('fizzzz buzzz')
+	}else if (i%5==0){
+		console.log('buzzz')
+	}else if(i%3==0){
+		console.log('fizz')
+	}else{
+		console.log(i)
+	}
+
+	
+}
